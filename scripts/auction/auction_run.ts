9616b4ca0d4c500446c53132107504e3e6ff5ba0
sewon <sewon@lovo.ai>
2022-10-05 11:41:38 +0900

    change hardhat to ethers

--- scripts/auction/auction_run.ts
+++ scripts/auction/auction_run.ts
@@ -1,27 +1,27 @@
 import { ethers } from "ethers";
 
 
-let privateKey = "0x0123456789012345678901234567890123456789012345678901234567890123";
+let PRIVATE_KEY = "0x4dfc9e11b48940aef89baf6a525fa7840caffd1cd3a2ccf6ec0cff78f8898ebe";
 const alchemyProvider = new ethers.providers.AlchemyProvider("goerli", "QSuGf9zqKJy_SxE6yKOwA1mMjzSw0m8B");
+let walletWithProvider = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
 
-let walletWithProvider = new ethers.Wallet(privateKey, alchemyProvider);
+const CONTRACT_ADDRESS = "0x54195c61528D4DC90Ae3dd943754ed3E63B60363";
 
-
-const CONTRACT_ADDRESS = "";
 let abi = [
+    "function auction() view external returns(uint256 alphaId, uint256 price, uint256 startTime, uint256 endTime, address payable bidder,bool settled)",
     "function settleCurrentAndCreateNewAuction() external"
 ];
 
-const vMAAuction = new ethers.Contract(CONTRACT_ADDRESS, abi, walletWithProvider);
+const vMAAuction_contract = new ethers.Contract(CONTRACT_ADDRESS, abi, walletWithProvider);
 
 async function main() {
 
-    // const auctionInfo = await vMAAuction.auction();
-    // console.log("The auctionInfo is: " + auctionInfo);
+    
+    const auctionInfo = await vMAAuction_contract.auction();
+    console.log("The auctionInfo is: " + auctionInfo);
 
-
-    const message = await vMAAuction.settleCurrentAndCreateNewAuction();
-    console.log("The message is: " + message);
+    // const message = await vMAAuction_contract.settleCurrentAndCreateNewAuction();
+    // console.log("The message is: " + JSON.stringify(message));
 
 }
 
