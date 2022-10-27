032cf5dd9d676df5573945bcd4eaf0cca7cb650d
sewon <sewon@lovo.ai>
2022-10-27 17:02:32 +0900

    add function transfer lockup

--- contracts/VoiceMaskAlpha.sol
+++ contracts/VoiceMaskAlpha.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
-import "hardhat/console.sol";
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IVoiceMaskAlpha} from "./interfaces/IVoiceMaskAlpha.sol";
 import {ERC721A} from "erc721a/contracts/ERC721A.sol";
+import {IERC721A} from "erc721a/contracts/IERC721A.sol";
 import {ERC721AQueryable} from "erc721a/contracts/extensions/ERC721AQueryable.sol";
 
 contract VoiceMaskAlpha is IVoiceMaskAlpha, ERC721A, ERC721AQueryable, Ownable {
@@ -13,9 +13,12 @@ contract VoiceMaskAlpha is IVoiceMaskAlpha, ERC721A, ERC721AQueryable, Ownable {
     address public minter;
     string private baseURI;
     uint256 public auctionSupply = 140;
-    uint256 public teamSupply = 60;
+    uint256 public teamSupply = 30;
     uint256 public auctionCount = 0;
     uint256 public teamCount = 0;
+
+    mapping(uint256 => uint256) public birthdayBlock; //token id, generated block
+    uint256 public durationBlock = (60 * 60 * 24 * 91) / 12; // 3 months
 
     modifier onlyMinter() {
         require(msg.sender == minter, "Sender is not the minter");
@@ -23,24 +26,21 @@ contract VoiceMaskAlpha is IVoiceMaskAlpha, ERC721A, ERC721AQueryable, Ownable {
     }
 
     function mintAuction() external onlyMinter returns (uint256) {
-        require(
-            auctionCount < auctionSupply,
-            "Auction supply all sold out"
-        );
+        require(auctionCount < auctionSupply, "Auction supply all sold out");
 
         auctionCount++;
-        return _mintTo(msg.sender, 1);
+        return _mintTo(msg.sender);
     }
 
-    function mintTeam(address to, uint256 quantity)
+    function mintTeam(address to, uint256 _quantity)
         external
         onlyOwner
         returns (uint256)
     {
-        require(teamCount + quantity <= teamSupply, "Team supply all sold out");
+        require(teamCount + 1 <= teamSupply, "Team supply all sold out");
 
         teamCount++;
-        return _mintTo(to, quantity);
+        return _mintTo(to);
     }
 
     function burn(uint256 alphaId) public override {
@@ -66,14 +66,63 @@ contract VoiceMaskAlpha is IVoiceMaskAlpha, ERC721A, ERC721AQueryable, Ownable {
         teamSupply = _maxMint;
     }
 
+    function getBirthdayBlock(uint256 _index) external view returns (uint256) {
+        return birthdayBlock[_index];
+    }
+
     function _baseURI() internal view override returns (string memory) {
         return baseURI;
     }
 
-    function _mintTo(address to, uint256 quantity) internal returns (uint256) {
-        _mint(to, quantity);
-        emit AlphaCreated(_totalMinted(), to);
+    function _mintTo(address to) internal returns (uint256) {
+        _mint(to, 1);
+
+        emit AlphaCreated(_nextTokenId() - 1, to);
+
+        //lock team supply only
+        if (_nextTokenId() - 1 <= 30) {
+            birthdayBlock[_nextTokenId() - 1] = block.number;
+        }
 
         return _nextTokenId() - 1;
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public payable override(ERC721A, IERC721A) {
+        require(!_checkIfLockedtoken(tokenId), "Token is locked up yet");
+        super.safeTransferFrom(from, to, tokenId, _data);
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public payable override(ERC721A, IERC721A) {
+        require(!_checkIfLockedtoken(tokenId), "Token is locked up yet");
+        super.safeTransferFrom(from, to, tokenId);
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public payable virtual override(ERC721A, IERC721A) {
+        require(!_checkIfLockedtoken(tokenId), "Token is locked up yet");
+        super.transferFrom(from, to , tokenId);
+    }
+
+    function _checkIfLockedtoken(uint256 token) internal view returns (bool) {
+        //lock team supply only
+        if (token >= teamSupply) {
+            return false;
+        }
+        if (birthdayBlock[token] + durationBlock < block.number) {
+            return false;
+        }
+        return true;
     }
 }
